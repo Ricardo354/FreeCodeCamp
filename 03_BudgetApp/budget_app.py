ff76872bd28594546add0cef9f7d032c6769c3fb
Ricardo354 <ricardotmd@protonmail.com>
2023-03-25 11:06:28 -0300

    adding basic methods

--- 03_BudgetApp/budget_app.py
+++ 03_BudgetApp/budget_app.py
@@ -1,4 +1,23 @@
 class Category:
-    pass
+    def __init__(self, description):
+        self.description = description
+        self.ledger = []
+        self.balance = 0
 
 
+    def deposit(self, amount, description=''):
+        self.balance += amount
+        self.ledger.append({"amount": amount, "description": description})
+        
+    def withdraw(amount, description):
+        if self.balance - amount >= 0:
+            self.ledger.append({'amount': -1 * amount, 'description':description})
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        return self.balance
+
+    def transfer(self, amount,  description):
+        pass
