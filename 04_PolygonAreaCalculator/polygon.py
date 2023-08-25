6535598e00dbe618d5c6a7418262588f71cd083f
Ricardo354 <ricardotmd@protonmail.com>
2023-08-24 22:05:43 -0300

    finishing square subclass and fixing typos

--- 04_PolygonAreaCalculator/polygon.py
+++ 04_PolygonAreaCalculator/polygon.py
@@ -1,29 +1,29 @@
 class Rectangle:
-    def __init__(self,width,height):
-        self.width = width
+    def __init__(self,widht,height):
+        self.widht = widht
         self.height = height 
 
-    def set_width(self, width):
-        self.width = width
+    def set_width(self, widht):
+        self.widht = widht
 
     def set_height(self,height):
         self.height = height
 
     def get_area(self):
-        return self.width * self.width
+        return self.widht * self.height
 
     def get_perimeter(self):
-        return 2 * self.width + 2 * self.height
+        return 2 * self.widht + 2 * self.height
 
     def get_diagonal(self):
-        return (self.width ** 2 + self.height ** 2) ** 0.5
+        return (self.widht ** 2 + self.height ** 2) ** 0.5
 
 
     def get_picture(self):
-        if self.width < 50 or self.height < 50:
+        if self.widht < 50 and self.height < 50:
             shape = ''
             for i in range(self.height):
-                shape += '*' * self.width + '\n'
+                shape += '*' * self.widht + '\n'
             return shape
         else:
             return 'Too big for picture.'
@@ -35,11 +35,18 @@ class Rectangle:
         return 0 
 
     def __str__(self):
-        return f'Rectangle(widht={self.width}, height={self.height})'
+        return f'Rectangle(width={self.widht}, height={self.height})'
 
 class Square(Rectangle):
     
     def __init__(self,side):
         super().__init__(side,side)
 
+    def set_side(self, side):
+        super().set_width(side)
+        super().set_height(side)
     
+    def __str__(self):
+        return f'Square(side={self.widht})'
+
+
